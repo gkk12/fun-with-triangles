04cde8ab2a0d60142153dd5620ca8d346a3ddecf
Gautham Kamath K <gauthamkamathk24@gmail.com>
2022-10-01 09:38:40 +0200

    handled invalid input values and used toFixed

--- hypotenuse.js
+++ hypotenuse.js
@@ -10,10 +10,20 @@ function calculateSumOfSquares(a,b){
 }
 
 function calculateHypotenuse(){
-    
-    const sumOfSquares = calculateSumOfSquares(Number(inputs[0].value), Number(inputs[1].value));
-
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-    //console.log(squaredValue);
-    outputText.innerText = "The length of the hypotenuse is : " +  lengthOfHypotenuse;
+    const firstSide = Number(inputs[0].value);
+    const secondSide = Number(inputs[1].value);
+    if(!firstSide || !secondSide){
+        outputText.innerText = "Please fill in all the fields"
+    }
+    else if(firstSide < 0 || secondSide <0)
+    {
+        outputText.innerText = "Please enter positive values"
+    }
+    else{
+        const sumOfSquares = calculateSumOfSquares(Number(inputs[0].value), Number(inputs[1].value));
+        const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+        //console.log(squaredValue);
+        outputText.innerText = `The length of your hypotenuse is ${Number.parseFloat(lengthOfHypotenuse).toFixed(2)}`;
+    }
+   
 }
--- istriangle.js
+++ istriangle.js
@@ -3,18 +3,26 @@ const checkIfTriangle = document.querySelector("#is-triangle-check");
 const outputText = document.querySelector("#output");
 
 function isTriangle() {
+
     const firstAngle = Number(angles[0].value);
     const secondAngle = Number(angles[1].value);
     const thirdAngle = Number(angles[2].value);
-    const sumOfAngles = calculateSumOfAngles(firstAngle, secondAngle, thirdAngle);
-    // console.log(sumOfAngles)
-
-    if (sumOfAngles == 180) {
-        outputText.innerText = "The given angles form a triangle 🚀"
-    } else {
-        outputText.innerText = "The given angles do not form a triangle 😪"
+    if(!firstAngle || !secondAngle || !thirdAngle){
+        outputText.innerText = "Please fill in all the fields"
     }
-
+    else if(firstAngle < 0 || secondAngle <0 || thirdAngle < 0)
+    {
+        outputText.innerText = "Please enter positive values"
+    }
+    else {
+        const sumOfAngles = calculateSumOfAngles(firstAngle, secondAngle, thirdAngle);
+        if (sumOfAngles == 180) {
+            outputText.innerText = "The given angles form a triangle 🚀"
+        } else {
+            outputText.innerText = "The given angles do not form a triangle 😪"
+        }
+    }
+    
 }
 
 function calculateSumOfAngles(angle1, angle2, angle3) {
